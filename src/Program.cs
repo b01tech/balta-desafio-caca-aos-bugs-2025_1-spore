7b416dd6c498679f6db12b6468dd8dc6f3fc7d52
b01tech <bruno01.tech@gmail.com>
2025-10-25 18:53:55 -0300

    feat: add database context configuration for SQLite

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,11 @@
+using BugStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
